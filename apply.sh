18dfdbf28351bb0960cd1af819c9852619d23870
Shareef Ali <shareefalis@gmail.com>
2012-03-04 22:46:22 -0500

    get rid of an obsolete patch.

--- apply.sh
+++ apply.sh
@@ -88,8 +88,6 @@ cdb
 
 repo start auto kernel/samsung/victory
 cdv kernel/samsung/victory
-echo "### Ignore IOCTL_MFC_BUF_CACHE requests, fixes decoded video artifacts. http://review.cyanogenmod.com/#change,13149"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/49/13149/4 && git cherry-pick FETCH_HEAD
 echo "### epicmtd: free unused mmap memory from older samsung powervr driver for gingerbread. result: 13 mb free http://review.cyanogenmod.com/#change,13284"
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/84/13284/1 && git cherry-pick FETCH_HEAD
 echo "### DockAudio: Add support for audio redirection to samsung docks with the help of the "Galaxy Dock Sound Redirector" market app. http://review.cyanogenmod.com/#change,13288"

9210a760b8c34221367a025dc6b9ebb46afd4822
Warren Togami <wtogami@gmail.com>
2012-03-08 21:11:32 -1000

    modular backuptool and sdcard ringtone were merged into CM9

--- apply.sh
+++ apply.sh
@@ -44,8 +44,6 @@ echo "### Patching Boot Animation ###"
 curl -L -o ./prebuilt/common/bootanimation.zip -O -L http://togami.com/~warren/temp/bootani-cm9-ver1-looponly-halfframe-16fps.zip
 git add prebuilt/common/bootanimation.zip
 git commit -m "DO NOT COMMIT TO GERRIT - Temporary Patch"
-echo "### Modular backuptool.sh. Executes backup and restore methods defined in arbitrary /system/addon.d/*.sh scripts. http://review.cyanogenmod.com/#change,13267"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_vendor_cm refs/changes/67/13267/5 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto device/samsung/epicmtd
@@ -67,12 +65,8 @@ git add telephony/java/android/telephony/SignalStrength.java
 git commit -m "DO NOT COMMIT TO GERRIT - Temporary Patch"
 cdb
 
-repo start auto packages/providers/MediaProvider
-cdv packages/providers/MediaProvider
-echo "### Check external storage volume ID to ensure media is actually mounted. http://review.cyanogenmod.com/13251"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_providers_MediaProvider refs/changes/51/13251/1 && git cherry-pick FETCH_HEAD
-echo "### Fix deletion of least-recently-used external databases. http://review.cyanogenmod.com/13280"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_providers_MediaProvider refs/changes/80/13280/2 && git cherry-pick FETCH_HEAD
+#repo start auto packages/providers/MediaProvider
+#cdv packages/providers/MediaProvider
 #echo "### Debug disappearing sdcard ringtones"
 #http_patch http://www.club.cc.cmu.edu/~mkasick/patches/packages_providers_MediaProvider_debug.diff
 #http_patch http://www.club.cc.cmu.edu/~mkasick/patches/packages_providers_MediaProvider_debug3.diff
@@ -80,7 +74,7 @@ git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_providers
 #git add src/com/android/providers/media/MediaProvider.java
 #rm -f src/com/android/providers/media/MediaProvider.java.orig
 #git commit -m "DO NOT COMMIT TO GERRIT - Temporary Patch"
-cdb
+#cdb
 
 repo start auto kernel/samsung/victory
 cdv kernel/samsung/victory
@@ -92,14 +86,3 @@ echo "### Epicmtd: add missing encyrpting support to kernel for device encyrptio
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/74/13374/1 && git cherry-pick FETCH_HEAD
 cdb
 
-repo start auto build
-cdv build
-echo "### Modular backuptool.sh. Executes backup and restore methods defined in arbitrary /system/addon.d/*.sh scripts. http://review.cyanogenmod.com/#change,13265"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_build refs/changes/65/13265/2 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto system/core
-cdv system/core
-echo "### Modular backuptool.sh. Executes backup and restore methods defined in arbitrary /system/addon.d/*.sh scripts. http://review.cyanogenmod.com/#change,13266"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_system_core refs/changes/66/13266/2 && git cherry-pick FETCH_HEAD
-cdb

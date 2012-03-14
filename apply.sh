b8897f5ef667176133151f08962efd9c9b372938
Warren Togami <wtogami@gmail.com>
2012-03-14 00:16:53 -1000

    Make it possible to dismiss the waiting voicemail notification.

--- apply.sh
+++ apply.sh
@@ -50,8 +50,6 @@ repo start auto device/samsung/epicmtd
 cdv device/samsung/epicmtd
 echo "### epicmtd: omx: mfc mmap buffer reduction to 22MB (included in beta0) http://review.cyanogenmod.com/#change,13319"
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_samsung_epicmtd refs/changes/19/13319/2 && git cherry-pick FETCH_HEAD
-echo "### epicmtd:  epicmtd: Use CDMA apns-conf.xml http://review.cyanogenmod.com/#change,13500"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_samsung_epicmtd refs/changes/00/13500/1 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto frameworks/base 
@@ -98,3 +96,8 @@ echo "### epicmtd: disable mdnie and disable useless devices that don't exist fo
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/33/13433/2 && git cherry-pick FETCH_HEAD
 cdb
 
+repo start auto packages/apps/Phone
+cdv packages/apps/Phone
+echo "### Make it possible to dismiss the waiting voicemail notification. http://review.cyanogenmod.com/#change,13592"
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_apps_Phone refs/changes/92/13592/2 && git cherry-pick FETCH_HEAD
+cdb

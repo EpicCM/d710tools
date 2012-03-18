24a9e38d09507e339805cefabdb72e7af9515ed9
Warren Togami <wtogami@gmail.com>
2012-03-17 23:35:40 -1000

    make better!

--- apply.sh
+++ apply.sh
@@ -41,9 +41,6 @@ repo abandon auto
 repo start auto vendor/cm/
 cdv vendor/cm/
 echo "### Patching Boot Animation ###"
-#curl -L -o ./prebuilt/common/bootanimation.zip -O -L http://togami.com/~warren/temp/bootani-cm9-ver1-looponly-halfframe-16fps.zip
-#git add prebuilt/common/bootanimation.zip
-#git commit -m "DO NOT COMMIT TO GERRIT - Temporary Patch - Boot Animation"
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_vendor_cm refs/changes/79/13579/3 && git cherry-pick FETCH_HEAD
 cdb
 
@@ -51,8 +48,6 @@ repo start auto device/samsung/epicmtd
 cdv device/samsung/epicmtd
 echo "### epicmtd: omx: mfc mmap buffer reduction to 22MB (included in beta0) http://review.cyanogenmod.com/#change,13319"
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_samsung_epicmtd refs/changes/19/13319/2 && git cherry-pick FETCH_HEAD
-#echo "### epicmtd: Use the samsung camera effects http://review.cyanogenmod.com/#change,13659"
-#git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_samsung_epicmtd refs/changes/59/13659/2 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto frameworks/base 
@@ -96,11 +91,11 @@ git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_vic
 echo "### Epicmtd: add missing encyrpting support to kernel for device encyrption http://review.cyanogenmod.com/#change,13374"
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/74/13374/1 && git cherry-pick FETCH_HEAD
 echo "### epicmtd: disable mdnie and disable useless devices that don't exist for this SOC. http://review.cyanogenmod.com/#change,13433"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/33/13433/2 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/33/13433/3 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto packages/apps/Phone
 cdv packages/apps/Phone
-echo "### Make it possible to dismiss the waiting voicemail notification. http://review.cyanogenmod.com/#change,13592"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_apps_Phone refs/changes/92/13592/2 && git cherry-pick FETCH_HEAD
+echo "### Phone: add voicemail notification setting http://review.cyanogenmod.com/#change,13706"
+git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_apps_Phone refs/changes/06/13706/2 && git cherry-pick FETCH_HEAD
 cdb

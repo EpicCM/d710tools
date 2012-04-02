e79b9addf00c85a8d5689ae2fcf3b95f39199316
Warren Togami <wtogami@gmail.com>
2012-04-01 21:21:31 -1000

    v3

--- apply.sh
+++ apply.sh
@@ -61,7 +61,7 @@ cdb
 repo start auto frameworks/base 
 cdv frameworks/base
 echo "### Test Patch: CDMA 1 signal bar threshold s/100/105/ to match Samsung"
-http_patch http://asgard.ancl.hawaii.edu/~warren/testonly-cdma-1bar-105-dBm-v2.patch
+http_patch http://asgard.ancl.hawaii.edu/~warren/testonly-cdma-1bar-105-dBm-v3.patch
 git add telephony/java/android/telephony/SignalStrength.java
 git commit -m "DO NOT COMMIT TO GERRIT - Temporary Patch"
 cdb

f81329d7c8473ce6dbd37b5340ce871c809d3bfa
Mike Kasick <mike@kasick.org>
2012-04-09 00:13:14 -0400

    Add headset-mic gain fix gerrit.

--- apply.sh
+++ apply.sh
@@ -85,7 +85,9 @@ cdv kernel/samsung/victory
 #git add Kernel/arch/arm/configs/cyanogenmod_epicmtd_defconfig
 #git commit -m "DO NOT COMMIT TO GERRIT - test CONFIG_FB_S3C_NR_BUFFERS=6"
 echo "### Adjust external-speaker playback-gain levels to match EL30. http://review.cyanogenmod.com/#change,14593"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/93/14593/1 && git cherry-pick FETCH_HEAD
+git fetch http://r.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/93/14593/2 && git cherry-pick FETCH_HEAD
+echo "### Adjust headset-mic voicecall-gain levels to match EL30. http://r.cyanogenmod.com/#change,14624"
+git fetch http://r.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/24/14624/1 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto packages/apps/Phone

7cdd735fb20fbeba1e3c1a75c2274d2c822d245a
Ameer Ghouse <aghouse1@gmail.com>
2012-05-16 21:46:51 -0500

    update signal patch to patchset 5

--- apply.sh
+++ apply.sh
@@ -68,7 +68,7 @@ cdb
 repo start auto frameworks/base
 cdv frameworks/base
 echo "### telephony: CDMA signal bar threshold s/100/105/ to match Samsung's behavior (DO NOT COMMIT) http://review.cyanogenmod.com/#/c/15580/"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/80/15580/4 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/80/15580/5 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto packages/apps/Phone

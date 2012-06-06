4e44af53de72cc699abe16dc539638f076debc93
Ameer Ghouse <aghouse1@gmail.com>
2012-06-05 19:16:17 -0500

    voicemail patch is now in upstream. remove it from script

--- apply.sh
+++ apply.sh
@@ -52,12 +52,6 @@ set -e
 
 ################ Apply Patches Below ####################
 
-cdv device/samsung/epic4gtouch
-echo "### patching config.xml for vmnotif in overlay/packages/apps/Phone/res/values/config.xml"
-git reset --hard
-http_patch http://darchstar.shabbypenguin.com/CM9/patch/vmnotif.patch
-cdb
-
 repo start auto packages/apps/Settings
 echo "### apply cpufreq patch so processor settings reads our cpufreq table properly... i should commit this to gerrit sometime soon"
 cdv packages/apps/Settings
@@ -69,16 +63,7 @@ repo start auto frameworks/base
 cdv frameworks/base
 echo "### telephony: CDMA signal bar threshold s/100/105/ to match Samsung's behavior (DO NOT COMMIT) http://review.cyanogenmod.com/#/c/15580/"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/80/15580/5 && git cherry-pick FETCH_HEAD
-echo "### Phone: Sprint MWI Quirk: Phantom message wait indicator workaround (2/2) http://review.cyanogenmod.com/#/c/16983/"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/83/16983/3 && git cherry-pick FETCH_HEAD
 cdb
-
-repo start auto packages/apps/Phone
-cdv packages/apps/Phone
-echo "### Phone: add voicemail notification setting http://review.cyanogenmod.com/#change,13706"
-git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_apps_Phone refs/changes/06/13706/11 && git cherry-pick FETCH_HEAD
-cdb
-
 
 ##### SUCCESS ####
 SUCCESS=true
